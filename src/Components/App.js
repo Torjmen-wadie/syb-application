4e7b812436cdfbe35efcb68e5d63ee33492440f9
Torjmen-wadie <58140901+Torjmen-wadie@users.noreply.github.com>
2022-08-23 17:41:13 +0200

    fisrt commit

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Block1 from './Block1/Blcok1';
+
+function App() {
+  return (
+    <div className="App">
+     <Block1/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Block1/Blcok1.js
@@ -0,0 +1,29 @@
+
+export default function Block1(){
+
+    return(
+        <div className="item1 row " id="item1">
+         
+         {/*   <span class="numberCircle"><span>1</span></span> */}
+
+         {/*   <h1 className='title1 '>Développer votre Cabinet <br/> avec un site web optimisé<br/> et un référencement performant !</h1>*/}
+         <div className="marg"> 
+         <div className='b1'>
+            <div className='ti1'>Développer votre Cabinet</div> 
+              <div className='ti2'> 
+                <span className='ti3'>avec un site web optimisé </span>
+                
+                
+             </div>
+             <div className='ti4'> 
+                <span className='ti5'>et un référencement performant !</span>
+             </div>
+        </div>
+
+         </div>
+         <div className=''>   
+	        <button className='button1 '>Je veux un site optimisé</button>
+        </div>
+        </div>
+    )
+}
